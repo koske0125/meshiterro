cee26bde74b8814f5c97c9afd34172602526d85b
koske0125 <79013492+koske0125@users.noreply.github.com>
2022-04-07 12:32:23 +0000

    [Fisnih]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users, onlu: [:show, :edit]
+  resources :users, only: [:show, :edit]
   resources :post_images, only: [:new, :index, :show, :create, :destroy]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
